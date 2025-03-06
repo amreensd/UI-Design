a30e7af2c5cef5fd0ff2185af59d79f9040440bc
amreensd <amreensd21@gmail.com>
2025-03-06 10:10:02 +0530

    Add files via upload

---
+++ LAB2/read.dart
@@ -0,0 +1,7 @@
+import 'dart:io';
+void main()
+{
+print("Enter the Namev  :");
+String name = stdin.readLineSync()!;
+stdout.write(name);
+}
---
+++ LAB2/str1.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+int sum(int a, int b) {
+  return a + b;
+}
+
+int sub(int a, int b) {
+  return a - b;
+}
+
+int multiply(int a, int b) {
+  return a * b;
+}
+
+int division(int a, int b) {
+  return a ~/ b;
+}
+y
+void main() {
+  print("Enter the first value: ");
+  int a = int.parse(stdin.readLineSync()!);   
+
+  print("Enter the second value: ");
+  int b = int.parse(stdin.readLineSync()!); 
+
+  print(sum(a, b));
+  print(sub(a, b));
+  print(multiply(a, b));
+  print(division(a, b)); 
+}
