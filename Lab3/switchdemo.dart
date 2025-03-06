00791df04cdffa0c5b8e3f6c84e7369e767c9906
amreensd <amreensd21@gmail.com>
2025-03-06 11:40:50 +0530

    Add files via upload

---
+++ Lab3/switchdemo.dart
@@ -0,0 +1,24 @@
+class  switchdemo{
+  void sdemo(String fruit) {
+ switch (fruit){
+   case 'Apple' || 'apple':
+           print("it is a fruit");
+	   
+    case 'Mango'|| 'mango':
+           print("it is a fruit");
+	   
+
+    case 'Grapes' || 'grapes':
+           print("it is a fruit");
+	   
+
+   case 'Watermelon' || 'watermelon ':
+           print("it is a fruit");
+	   
+  default:	
+	   print("Sorry fruit is not listed");
+           
+
+    }
+  }
+}
